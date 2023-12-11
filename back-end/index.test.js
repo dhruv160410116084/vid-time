f43ad8e086fa9c7c22a3dcbf508802b1e00511ed
dhruv <dhruv.160410116084@gmail.com>
2023-12-11 13:20:58 -0500

    test file updated

--- back-end/index.test.js
+++ back-end/index.test.js
@@ -1,4 +1,4 @@
-const subtraction = require('./index');
+const subtraction = require('./index')
 test('subtracts 4 - 2 to equal 2', () => {
-   expect(subtraction(4, 2)).toBe(2);
-});
+   expect(subtraction(4, 2)).toBe(2)
+})
